62dd26ae887e7d48220534b22f21e8352c187912
alfredjmg <alfredomujicagodoy@gmail.com>
2019-10-06 14:52:36 -0400

    variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://root:root123@cluster0-bltuz.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
