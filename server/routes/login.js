cd4cbf5bb70d7e372acc8aefeecf6a633bf7c301
alfredjmg <alfredomujicagodoy@gmail.com>
2019-10-20 16:55:44 -0400

    login

--- server/routes/login.js
+++ server/routes/login.js
@@ -10,8 +10,41 @@ module.exports = app;
 
 app.post('/login', (req,res) => {
 
-    res.json({
-        ok:true
-    });
+    let body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB)=>{
+
+        if( err ){
+            return res.status(500).json({
+                success: false,
+                err
+            });
+        }
+
+        if(!usuarioDB){
+            return res.status(400).json({
+                success: false,
+                err:{
+                    message: '(Usuario) o contraseña incorrectos'
+                }
+            });
+        }
+
+        if( !bcrypt.compareSync( body.password, usuarioDB.password ) ){
+            return res.status(400).json({
+                success: false,
+                err:{
+                    message: 'Usuario o (contraseña) incorrectos'
+                }
+            });
+        }
+
+        res.json({
+            ok:true,
+            usuario: usuarioDB,
+            token: '123'
+        });
+
+    })
 
 });
